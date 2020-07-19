83439986e5d3a6c1599bab962c33f23acd67fe7b
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-19 22:22:37 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,16 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        w: f64,
+        h: f64,
+        x: f64,
+        y: f64
+    }
+    let rec: f64 = w * h / 2.0;
+
+    if x == w / 2.0 && y == h / 2.0 {
+        println!("{} {}", rec, 1);
+    } else {
+        println!("{} {}", rec, 0);
     }
 }
